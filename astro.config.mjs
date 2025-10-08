4cc98f06afb247320bd3e6f3ad84c6074acd43be
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-10-08 13:01:27 +0200

    feat: :lipstick: style the index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        name: "Clash Display",
+        cssVariable: "--font-clash-display",
+        provider: fontProviders.fontshare(),
+      },
+      {
+        name: "Satoshi",
+        cssVariable: "--font-satoshi",
+        provider: fontProviders.fontshare(),
+      },
+    ],
   },
 });
