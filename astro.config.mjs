6269c1acb1ae83762e1addd00008891ade9d592a
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-10-08 13:25:56 +0200

    feat: :lipstick: add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   experimental: {
     fonts: [
       {
@@ -21,4 +24,6 @@ export default defineConfig({
       },
     ],
   },
+
+  integrations: [icon()],
 });
